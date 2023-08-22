33fdef7228bfc23198e37ac3ebfcaf16bf0cf950
g1kwon <spring_shot@naver.com>
2023-08-23 01:08:19 +0900

    initial commit

---
+++ src/test/java/AluTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+class AluTest {
+
+    @Test
+    public void getCal() {
+        Alu alu = new Alu();
+        alu.setOperand1(10);
+        alu.setOperand2(20);
+        alu.setOPCODE("ADD");
+
+        result ret = new result();
+        alu.enableSignal(ret);
+
+        assertEquals(30, ret.getResult());
+        assertEquals(0, ret.getStatus());
+    }
+
+}
